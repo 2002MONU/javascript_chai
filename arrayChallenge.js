f50cfa26231edc963b83007d226f009d9ff2c5bb
2002MONU <monusaini1663197@gmail.com>
2025-03-21 21:33:35 +0530

    new coce

---
+++ arrayChallenge.js
@@ -0,0 +1,49 @@
+
+let teaFlavour = ['green tea', 'black tea','oolang tea'];
+
+//let teaFl = new Array('green tea', 'black tea','oolang tea');
+const firstTea = teaFlavour[0];
+
+// console.log(firstTea);
+
+let cities = ['Tokya','Paris','New your'];
+
+const fabourCity = cities['2'];
+
+// console.log(fabourCity);
+
+let teaTypes = ["herbal teA", "White Tea","masala tea"];
+teaTypes[1]= "jesime";
+
+// console.log(teaTypes);
+
+let cityVisited = ["Mumbai","Sydney"];
+cityVisited.push('Berlin');
+
+// console.log(cityVisited);
+
+let teaOrder = ["chai","iced tea","matcha","earl grey"];
+
+teaOrder.pop();
+
+// console.log(teaOrder);
+
+ let popularCity = ["green tea","oolang tea","chai"];
+ let softCopyTea = popularCity
+
+//  console.log(softCopyTea);
+ 
+let europeanCities = ["London","Paris","Berlin"];
+let asianCities = ["Tokyo","Seoul","Bangkok"];
+
+// let allCities = europeanCities +asianCities;
+// let allCities = [...europeanCities,...asianCities]; // spread operator in ES6
+let allCities = europeanCities.concat(asianCities); // spread operator in ES6
+
+// console.log(allCities);
+
+let teaMenu  = ['chai','green tea','oolang tea'];
+let menuCopy = teaMenu.length;
+
+// console.log(menuCopy);
+
